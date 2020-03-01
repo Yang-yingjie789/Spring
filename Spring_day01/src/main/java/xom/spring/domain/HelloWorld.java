9939ccdf1da0114b32b4001446b633662594073b
Yang Yingjie <stephen_yang789@126.com>
2020-03-01 23:18:10 +0800

    提交Spring 学习项目到 本地仓库

---
+++ Spring_day01/src/main/java/xom/spring/domain/HelloWorld.java
@@ -0,0 +1,17 @@
+package xom.spring.domain;
+
+public class HelloWorld {
+    private String name;
+
+   public void hello(){
+       System.out.println("hello "+name);
+   }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Spring_day01/src/main/java/xom/spring/test/Demo1.java
@@ -0,0 +1,11 @@
+package xom.spring.test;
+
+import xom.spring.domain.HelloWorld;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        HelloWorld helloWorld=new HelloWorld();
+        helloWorld.setName("Japan");
+        helloWorld.hello();
+    }
+}
