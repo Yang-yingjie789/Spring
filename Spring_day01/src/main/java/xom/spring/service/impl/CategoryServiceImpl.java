d466b0e6d28d09a1076550c2cf8516054b669658
Yang Yingjie <stephen_yang789@126.com>
2020-03-02 23:44:00 +0800

    配置Bean的方式减低耦合

--- Spring_day01/src/main/java/xom/spring/service/impl/CategoryServiceImpl.java
+++ Spring_day01/src/main/java/xom/spring/service/impl/CategoryServiceImpl.java
@@ -2,10 +2,11 @@ package xom.spring.service.impl;
 
 import xom.spring.dao.CategoryDao;
 import xom.spring.dao.impl.CategoryDaoImpl;
+import xom.spring.factory.Beanfactory;
 import xom.spring.service.CategoryService;
 
 public class CategoryServiceImpl implements CategoryService {
-    private CategoryDao categoryDao=new CategoryDaoImpl();
+    private CategoryDao categoryDao= (CategoryDao) new Beanfactory().getBean("categoryDao");
     public void saveCategory() {
          categoryDao.saveCategory();
     }
--- Spring_day01/src/main/java/xom/spring/web/ClientDemo1.java
+++ Spring_day01/src/main/java/xom/spring/web/ClientDemo1.java
@@ -1,10 +1,11 @@
 package xom.spring.web;
 
+import xom.spring.factory.Beanfactory;
 import xom.spring.service.CategoryService;
 import xom.spring.service.impl.CategoryServiceImpl;
 
 public class ClientDemo1 {
-    private static CategoryService  categoryService=new CategoryServiceImpl();
+    private static CategoryService  categoryService= (CategoryService) new Beanfactory().getBean("categoryService");
     public static void main(String[] args) {
        categoryService.saveCategory();
     }
